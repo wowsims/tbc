05901c3a55c0867e9451643bcd47d08b379d0b2f
Ben Echols <lologarithm@gmail.com>
2021-09-25 22:21:05 -0600

    starting work on stats and eps

---
+++ sim/runner/statweight_test.go
@@ -0,0 +1,41 @@
+package runner
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/wowsims/tbc/sim/core"
+	"github.com/wowsims/tbc/sim/shaman"
+)
+
+func TestCalcStatWeight(t *testing.T) {
+	options := basicOptions
+	options.Iterations = 1000
+	options.Encounter = shortEncounter
+
+	params := IndividualParams{
+		Equip:       gearFromStrings(p1Gear),
+		Race:        core.RaceBonusTypeTroll10,
+		Consumes:    fullConsumes,
+		Buffs:       fullBuffs,
+		Options:     options,
+		Spec:        shaman.ElementalSpec{Talents: shamTalents, Totems: shamTotems, AgentID: shaman.AgentTypeAdaptive},
+		CustomStats: []float64{},
+	}
+
+	tests := []struct {
+		name   string
+		params IndividualParams
+		want   StatWeightsResult
+	}{
+		{name: "First Test", params: params, want: StatWeightsResult{}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalcStatWeight(tt.params); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CalcStatWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
