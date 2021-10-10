f6e07b63fe6d221115809d8ec6883ff671c115a2
James Tanner <jimmyt857@gmail.com>
2021-10-09 20:18:59 -0400

    Fix api so all the tests pass

--- sim/api_impl.go
+++ sim/api_impl.go
@@ -102,7 +102,8 @@ func runSimulationImpl(request *proto.IndividualSimRequest) *proto.IndividualSim
 	result := sim.Run()
 
 	actionMetrics := []*proto.ActionMetric{}
-	for _, v := range result.Actions {
+	// TODO: Actually return results for all agents
+	for _, v := range result.Agents[0].Actions {
 		metric := &proto.ActionMetric{
 			Casts:  v.Casts,
 			Crits:  v.Crits,
@@ -118,15 +119,15 @@ func runSimulationImpl(request *proto.IndividualSimRequest) *proto.IndividualSim
 		actionMetrics = append(actionMetrics, metric)
 	}
 	isr := &proto.IndividualSimResult{
-		DpsAvg:              result.DpsAvg,
-		DpsStdev:            result.DpsStDev,
-		DpsHist:             result.DpsHist,
+		DpsAvg:              result.Agents[0].DpsAvg,
+		DpsStdev:            result.Agents[0].DpsStDev,
+		DpsHist:             result.Agents[0].DpsHist,
 		Logs:                result.Logs,
-		DpsMax:              result.DpsMax,
+		DpsMax:              result.Agents[0].DpsMax,
 		ExecutionDurationMs: result.ExecutionDurationMs,
-		NumOom:              int32(result.NumOom),
-		OomAtAvg:            result.OomAtAvg,
-		DpsAtOomAvg:         result.DpsAtOomAvg,
+		NumOom:              int32(result.Agents[0].NumOom),
+		OomAtAvg:            result.Agents[0].OomAtAvg,
+		DpsAtOomAvg:         result.Agents[0].DpsAtOomAvg,
 		ActionMetrics:       actionMetrics,
 	}
 	return isr
