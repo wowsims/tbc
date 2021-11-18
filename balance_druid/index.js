fbece96ba1b6d2038732a544a4a243c651138590
jimmyt857 <jimmyt857@gmail.com>
2021-11-18 02:52:09 +0000

    Deploying to gh-pages from @ wowsims/tbc@54f5c7d464360170717746c4ed055c46c7e09b4a 🚀

--- balance_druid/index.js
+++ balance_druid/index.js
@@ -59,14 +59,14 @@ const theme = new DefaultTheme(document.body, {
             gear: Presets.P1_ALLIANCE_BIS.gear,
             // Default EP weights for sorting gear in the gear picker.
             epWeights: Stats.fromMap({
-                [Stat.StatIntellect]: 0.65,
+                [Stat.StatIntellect]: 0.54,
                 [Stat.StatSpirit]: 0.1,
                 [Stat.StatSpellPower]: 1,
                 [Stat.StatArcaneSpellPower]: 1,
                 [Stat.StatNatureSpellPower]: 0,
-                [Stat.StatSpellCrit]: 0.9,
-                [Stat.StatSpellHaste]: 1.34,
-                [Stat.StatMP5]: 0.02,
+                [Stat.StatSpellCrit]: 0.84,
+                [Stat.StatSpellHaste]: 1.29,
+                [Stat.StatMP5]: 0.00,
             }),
             // Default consumes settings.
             consumes: Consumes.create({
