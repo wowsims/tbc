34baddce3c55a099870d33f29aad63de1bdb87ec
Michael Coppinger <michaelecoppinger@gmail.com>
2021-11-21 10:55:53 -0800

    Adding additional mana cost and time for shifting back into moonkin form

--- sim/druid/rebirth.go
+++ sim/druid/rebirth.go
@@ -6,8 +6,11 @@ import (
 	"github.com/wowsims/tbc/sim/core"
 )
 
+// Right now, add the additional GCD + mana cost for shifting back to Moonkin form as a hack
+// Consider adding moonkin shapeshift spell / form tracking to balance rotation instead
+// Then we can properly incur Rebirth cost through additional Moonkin form spell cast
 func (druid *Druid) NewRebirth(sim *core.Simulation) *core.SimpleCast {
-	var manaCost float64 = 1611
+	var manaCost float64 = 1611 + (521.4 * (1 - (float64(druid.Talents.NaturalShapeshifter) *  0.1)))
 
 	rb := &core.SimpleCast {
 		Cast: core.Cast{
@@ -16,7 +19,7 @@ func (druid *Druid) NewRebirth(sim *core.Simulation) *core.SimpleCast {
 			Character:       druid.GetCharacter(),
 			BaseManaCost:    manaCost,
 			ManaCost:        manaCost,
-			CastTime:        time.Second * 2,
+			CastTime:        time.Second * 3 + time.Millisecond * 500,
 		},
 		OnCastComplete: func(sim *core.Simulation, cast *core.Cast) { druid.RebirthUsed = true },
 	}
