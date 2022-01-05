0e2871a61161237afe6eb8068ecbfbc85c98220b
Ben Echols <lologarithm@gmail.com>
2022-01-05 00:31:27 -0700

    a bunch of enhancement items

--- sim/common/melee_trinkets.go
+++ sim/common/melee_trinkets.go
@@ -10,6 +10,7 @@ import (
 func init() {
 	// Proc effects. Keep these in order by item ID.
 	core.AddItemEffect(28830, ApplyDragonspineTrophy)
+	core.AddItemEffect(28034, ApplyHourglassUnraveller)
 
 	// Activatable effects. Keep these in order by item ID.
 	var BloodlustBroochCooldownID = core.NewCooldownID()
@@ -23,6 +24,38 @@ func init() {
 		core.MajorCooldown{
 			ActionID:         core.ActionID{ItemID: 29383},
 			CooldownID:       BloodlustBroochCooldownID,
+			Cooldown:         time.Minute * 2,
+			SharedCooldownID: core.OffensiveTrinketSharedCooldownID,
+		},
+	))
+
+	var AbacusViolentOddsCooldownID = core.NewCooldownID()
+	core.AddItemEffect(28288, core.MakeTemporaryStatsOnUseCDRegistration(
+		core.OffensiveTrinketActiveAuraID,
+		33807,
+		"Haste",
+		stats.MeleeHaste,
+		260,
+		time.Second*10,
+		core.MajorCooldown{
+			ActionID:         core.ActionID{ItemID: 28288},
+			CooldownID:       AbacusViolentOddsCooldownID,
+			Cooldown:         time.Minute * 2,
+			SharedCooldownID: core.OffensiveTrinketSharedCooldownID,
+		},
+	))
+
+	var EmptyDirebrewMugCooldownID = core.NewCooldownID()
+	core.AddItemEffect(38287, core.MakeTemporaryStatsOnUseCDRegistration(
+		core.OffensiveTrinketActiveAuraID,
+		51955,
+		"Dire Drunkard",
+		stats.AttackPower,
+		278,
+		time.Second*20,
+		core.MajorCooldown{
+			ActionID:         core.ActionID{ItemID: 38287},
+			CooldownID:       EmptyDirebrewMugCooldownID,
 			Cooldown:         time.Minute * 2,
 			SharedCooldownID: core.OffensiveTrinketSharedCooldownID,
 		},
@@ -62,6 +95,44 @@ func ApplyDragonspineTrophy(agent core.Agent) {
 				}
 				icd = core.InternalCD(sim.CurrentTime + icdDur)
 				character.AddAuraWithTemporaryStats(sim, MeleeHasteAuraID, 34775, "Haste", stats.MeleeHaste, hasteBonus, dur)
+			},
+		}
+	})
+}
+
+var HourglassUnravellerAuraID = core.NewAuraID()
+var RageOfUnravellerAuraID = core.NewAuraID()
+
+func ApplyHourglassUnraveller(agent core.Agent) {
+	character := agent.GetCharacter()
+	character.AddPermanentAura(func(sim *core.Simulation) core.Aura {
+		icd := core.NewICD()
+		const statBonus = 300.0
+		const dur = time.Second * 10
+		const icdDur = time.Second * 50
+
+		procChance, ohProcChance := core.PPMToChance(character, 1.0)
+		return core.Aura{
+			ID:   HourglassUnravellerAuraID,
+			Name: "Hourglass of the Unraveller",
+			OnMeleeAttack: func(sim *core.Simulation, target *core.Target, result core.MeleeHitType, ability *core.ActiveMeleeAbility, isOH bool) {
+				if result != core.MeleeHitTypeCrit {
+					return
+				}
+				if icd.IsOnCD(sim) {
+					return // dont activate
+				}
+				if !isOH {
+					if sim.RandomFloat("hourglass") > procChance {
+						return // didn't proc
+					}
+				} else {
+					if sim.RandomFloat("hourglass") > ohProcChance {
+						return // didn't proc
+					}
+				}
+				icd = core.InternalCD(sim.CurrentTime + icdDur)
+				character.AddAuraWithTemporaryStats(sim, RageOfUnravellerAuraID, 33648, "Rage of the Unraveller", stats.AttackPower, statBonus, dur)
 			},
 		}
 	})
