5080b8aa3e0cf5591385e7f781cb72acfaa9a038
jimmyt857 <jimmyt857@gmail.com>
2022-01-09 01:33:46 +0000

    Deploying to gh-pages from @ wowsims/tbc@d2da74e4d323fc2047fcd7c8133cdeb581aafe1c 🚀

--- balance_druid/sim.js
+++ balance_druid/sim.js
@@ -166,7 +166,7 @@ export class BalanceDruidSimUI extends IndividualSimUI {
                 // Whether to include 'Execute Duration (%)' in the 'Encounter' section of the settings tab.
                 showExecuteProportion: false,
                 // Whether to include 'Num Targets' in the 'Encounter' section of the settings tab.
-                showNumTargets: true,
+                showNumTargets: false,
             },
             // If true, the talents on the talents tab will not be individually modifiable by the user.
             // Note that the use can still pick between preset talents, if there is more than 1.
--- mage/sim.js
+++ mage/sim.js
@@ -174,7 +174,7 @@ export class MageSimUI extends IndividualSimUI {
                 // Whether to include 'Execute Duration (%)' in the 'Encounter' section of the settings tab.
                 showExecuteProportion: true,
                 // Whether to include 'Num Targets' in the 'Encounter' section of the settings tab.
-                showNumTargets: true,
+                showNumTargets: false,
             },
             // If true, the talents on the talents tab will not be individually modifiable by the user.
             // Note that the use can still pick between preset talents, if there is more than 1.
