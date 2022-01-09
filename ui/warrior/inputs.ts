f2004fd8ad63fe13480670ddc07673b6eb195930
The Grox Empire <47816228+TheGroxEmpire@users.noreply.github.com>
2022-01-09 16:37:43 +0700

    Updated Reck spellID to appropriate fromSpellId function

--- ui/warrior/inputs.ts
+++ ui/warrior/inputs.ts
@@ -2,7 +2,7 @@ import { IconPickerConfig } from '/tbc/core/components/icon_picker.js';
 import { RaidTarget } from '/tbc/core/proto/common.js';
 import { Spec } from '/tbc/core/proto/common.js';
 import { NO_TARGET } from '/tbc/core/proto_utils/utils.js';
-import { ItemOrSpellId } from '/tbc/core/proto_utils/action_id.js';
+import { ActionId } from '/tbc/core/proto_utils/action_id.js';
 import { Player } from '/tbc/core/player.js';
 import { Sim } from '/tbc/core/sim.js';
 import { EventID, TypedEvent } from '/tbc/core/typed_event.js';
@@ -19,7 +19,7 @@ import * as Presets from './presets.js';
 // These don't need to be in a separate file but it keeps things cleaner.
 
 export const Recklessness = {
-	id: { spellId: 1719},
+	id: ActionId.fromSpellId(1719),
 	states: 2,
 	extraCssClasses: [
 		'warrior-recklessness-picker',
