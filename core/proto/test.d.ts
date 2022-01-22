b34eac859fae6b5a050af5a7a639dab05cc3e3db
jimmyt857 <jimmyt857@gmail.com>
2022-01-22 05:16:36 +0000

    Deploying to gh-pages from @ wowsims/tbc@b48f915efeb09d902459216a25336810a01aae4c 🚀

--- core/proto/test.d.ts
+++ core/proto/test.d.ts
@@ -5,11 +5,29 @@ import type { IBinaryReader } from "@protobuf-ts/runtime";
 import type { PartialMessage } from "@protobuf-ts/runtime";
 import { MessageType } from "@protobuf-ts/runtime";
 /**
+ * @generated from protobuf message proto.CharacterStatsTestResult
+ */
+export interface CharacterStatsTestResult {
+    /**
+     * @generated from protobuf field: repeated double final_stats = 1;
+     */
+    finalStats: number[];
+}
+/**
+ * @generated from protobuf message proto.StatWeightsTestResult
+ */
+export interface StatWeightsTestResult {
+    /**
+     * @generated from protobuf field: repeated double weights = 1;
+     */
+    weights: number[];
+}
+/**
  * @generated from protobuf message proto.DpsTestResult
  */
 export interface DpsTestResult {
     /**
-     * @generated from protobuf field: double dps = 2;
+     * @generated from protobuf field: double dps = 1;
      */
     dps: number;
 }
@@ -20,12 +38,48 @@ export interface TestSuiteResult {
     /**
      * Maps test names to their results.
      *
+     * @generated from protobuf field: map<string, proto.CharacterStatsTestResult> character_stats_results = 2;
+     */
+    characterStatsResults: {
+        [key: string]: CharacterStatsTestResult;
+    };
+    /**
+     * Maps test names to their results.
+     *
+     * @generated from protobuf field: map<string, proto.StatWeightsTestResult> stat_weights_results = 3;
+     */
+    statWeightsResults: {
+        [key: string]: StatWeightsTestResult;
+    };
+    /**
+     * Maps test names to their results.
+     *
      * @generated from protobuf field: map<string, proto.DpsTestResult> dps_results = 1;
      */
     dpsResults: {
         [key: string]: DpsTestResult;
     };
 }
+declare class CharacterStatsTestResult$Type extends MessageType<CharacterStatsTestResult> {
+    constructor();
+    create(value?: PartialMessage<CharacterStatsTestResult>): CharacterStatsTestResult;
+    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: CharacterStatsTestResult): CharacterStatsTestResult;
+    internalBinaryWrite(message: CharacterStatsTestResult, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter;
+}
+/**
+ * @generated MessageType for protobuf message proto.CharacterStatsTestResult
+ */
+export declare const CharacterStatsTestResult: CharacterStatsTestResult$Type;
+declare class StatWeightsTestResult$Type extends MessageType<StatWeightsTestResult> {
+    constructor();
+    create(value?: PartialMessage<StatWeightsTestResult>): StatWeightsTestResult;
+    internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: StatWeightsTestResult): StatWeightsTestResult;
+    internalBinaryWrite(message: StatWeightsTestResult, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter;
+}
+/**
+ * @generated MessageType for protobuf message proto.StatWeightsTestResult
+ */
+export declare const StatWeightsTestResult: StatWeightsTestResult$Type;
 declare class DpsTestResult$Type extends MessageType<DpsTestResult> {
     constructor();
     create(value?: PartialMessage<DpsTestResult>): DpsTestResult;
@@ -40,6 +94,8 @@ declare class TestSuiteResult$Type extends MessageType<TestSuiteResult> {
     constructor();
     create(value?: PartialMessage<TestSuiteResult>): TestSuiteResult;
     internalBinaryRead(reader: IBinaryReader, length: number, options: BinaryReadOptions, target?: TestSuiteResult): TestSuiteResult;
+    private binaryReadMap2;
+    private binaryReadMap3;
     private binaryReadMap1;
     internalBinaryWrite(message: TestSuiteResult, writer: IBinaryWriter, options: BinaryWriteOptions): IBinaryWriter;
 }
--- core/proto/test.js
+++ core/proto/test.js
@@ -1,13 +1,123 @@
-import { WireType } from '/tbc/protobuf-ts/index.js';
 import { UnknownFieldHandler } from '/tbc/protobuf-ts/index.js';
+import { WireType } from '/tbc/protobuf-ts/index.js';
 import { reflectionMergePartial } from '/tbc/protobuf-ts/index.js';
 import { MESSAGE_TYPE } from '/tbc/protobuf-ts/index.js';
 import { MessageType } from '/tbc/protobuf-ts/index.js';
 // @generated message type with reflection information, may provide speed optimized methods
+class CharacterStatsTestResult$Type extends MessageType {
+    constructor() {
+        super("proto.CharacterStatsTestResult", [
+            { no: 1, name: "final_stats", kind: "scalar", repeat: 1 /*RepeatType.PACKED*/, T: 1 /*ScalarType.DOUBLE*/ }
+        ]);
+    }
+    create(value) {
+        const message = { finalStats: [] };
+        Object.defineProperty(message, MESSAGE_TYPE, { enumerable: false, value: this });
+        if (value !== undefined)
+            reflectionMergePartial(this, message, value);
+        return message;
+    }
+    internalBinaryRead(reader, length, options, target) {
+        let message = target ?? this.create(), end = reader.pos + length;
+        while (reader.pos < end) {
+            let [fieldNo, wireType] = reader.tag();
+            switch (fieldNo) {
+                case /* repeated double final_stats */ 1:
+                    if (wireType === WireType.LengthDelimited)
+                        for (let e = reader.int32() + reader.pos; reader.pos < e;)
+                            message.finalStats.push(reader.double());
+                    else
+                        message.finalStats.push(reader.double());
+                    break;
+                default:
+                    let u = options.readUnknownField;
+                    if (u === "throw")
+                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
+                    let d = reader.skip(wireType);
+                    if (u !== false)
+                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
+            }
+        }
+        return message;
+    }
+    internalBinaryWrite(message, writer, options) {
+        /* repeated double final_stats = 1; */
+        if (message.finalStats.length) {
+            writer.tag(1, WireType.LengthDelimited).fork();
+            for (let i = 0; i < message.finalStats.length; i++)
+                writer.double(message.finalStats[i]);
+            writer.join();
+        }
+        let u = options.writeUnknownFields;
+        if (u !== false)
+            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
+        return writer;
+    }
+}
+/**
+ * @generated MessageType for protobuf message proto.CharacterStatsTestResult
+ */
+export const CharacterStatsTestResult = new CharacterStatsTestResult$Type();
+// @generated message type with reflection information, may provide speed optimized methods
+class StatWeightsTestResult$Type extends MessageType {
+    constructor() {
+        super("proto.StatWeightsTestResult", [
+            { no: 1, name: "weights", kind: "scalar", repeat: 1 /*RepeatType.PACKED*/, T: 1 /*ScalarType.DOUBLE*/ }
+        ]);
+    }
+    create(value) {
+        const message = { weights: [] };
+        Object.defineProperty(message, MESSAGE_TYPE, { enumerable: false, value: this });
+        if (value !== undefined)
+            reflectionMergePartial(this, message, value);
+        return message;
+    }
+    internalBinaryRead(reader, length, options, target) {
+        let message = target ?? this.create(), end = reader.pos + length;
+        while (reader.pos < end) {
+            let [fieldNo, wireType] = reader.tag();
+            switch (fieldNo) {
+                case /* repeated double weights */ 1:
+                    if (wireType === WireType.LengthDelimited)
+                        for (let e = reader.int32() + reader.pos; reader.pos < e;)
+                            message.weights.push(reader.double());
+                    else
+                        message.weights.push(reader.double());
+                    break;
+                default:
+                    let u = options.readUnknownField;
+                    if (u === "throw")
+                        throw new globalThis.Error(`Unknown field ${fieldNo} (wire type ${wireType}) for ${this.typeName}`);
+                    let d = reader.skip(wireType);
+                    if (u !== false)
+                        (u === true ? UnknownFieldHandler.onRead : u)(this.typeName, message, fieldNo, wireType, d);
+            }
+        }
+        return message;
+    }
+    internalBinaryWrite(message, writer, options) {
+        /* repeated double weights = 1; */
+        if (message.weights.length) {
+            writer.tag(1, WireType.LengthDelimited).fork();
+            for (let i = 0; i < message.weights.length; i++)
+                writer.double(message.weights[i]);
+            writer.join();
+        }
+        let u = options.writeUnknownFields;
+        if (u !== false)
+            (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
+        return writer;
+    }
+}
+/**
+ * @generated MessageType for protobuf message proto.StatWeightsTestResult
+ */
+export const StatWeightsTestResult = new StatWeightsTestResult$Type();
+// @generated message type with reflection information, may provide speed optimized methods
 class DpsTestResult$Type extends MessageType {
     constructor() {
         super("proto.DpsTestResult", [
-            { no: 2, name: "dps", kind: "scalar", T: 1 /*ScalarType.DOUBLE*/ }
+            { no: 1, name: "dps", kind: "scalar", T: 1 /*ScalarType.DOUBLE*/ }
         ]);
     }
     create(value) {
@@ -22,7 +132,7 @@ class DpsTestResult$Type extends MessageType {
         while (reader.pos < end) {
             let [fieldNo, wireType] = reader.tag();
             switch (fieldNo) {
-                case /* double dps */ 2:
+                case /* double dps */ 1:
                     message.dps = reader.double();
                     break;
                 default:
@@ -37,9 +147,9 @@ class DpsTestResult$Type extends MessageType {
         return message;
     }
     internalBinaryWrite(message, writer, options) {
-        /* double dps = 2; */
+        /* double dps = 1; */
         if (message.dps !== 0)
-            writer.tag(2, WireType.Bit64).double(message.dps);
+            writer.tag(1, WireType.Bit64).double(message.dps);
         let u = options.writeUnknownFields;
         if (u !== false)
             (u == true ? UnknownFieldHandler.onWrite : u)(this.typeName, message, writer);
@@ -54,11 +164,13 @@ export const DpsTestResult = new DpsTestResult$Type();
 class TestSuiteResult$Type extends MessageType {
     constructor() {
         super("proto.TestSuiteResult", [
+            { no: 2, name: "character_stats_results", kind: "map", K: 9 /*ScalarType.STRING*/, V: { kind: "message", T: () => CharacterStatsTestResult } },
+            { no: 3, name: "stat_weights_results", kind: "map", K: 9 /*ScalarType.STRING*/, V: { kind: "message", T: () => StatWeightsTestResult } },
             { no: 1, name: "dps_results", kind: "map", K: 9 /*ScalarType.STRING*/, V: { kind: "message", T: () => DpsTestResult } }
         ]);
     }
     create(value) {
-        const message = { dpsResults: {} };
+        const message = { characterStatsResults: {}, statWeightsResults: {}, dpsResults: {} };
         Object.defineProperty(message, MESSAGE_TYPE, { enumerable: false, value: this });
         if (value !== undefined)
             reflectionMergePartial(this, message, value);
@@ -69,6 +181,12 @@ class TestSuiteResult$Type extends MessageType {
         while (reader.pos < end) {
             let [fieldNo, wireType] = reader.tag();
             switch (fieldNo) {
+                case /* map<string, proto.CharacterStatsTestResult> character_stats_results */ 2:
+                    this.binaryReadMap2(message.characterStatsResults, reader, options);
+                    break;
+                case /* map<string, proto.StatWeightsTestResult> stat_weights_results */ 3:
+                    this.binaryReadMap3(message.statWeightsResults, reader, options);
+                    break;
                 case /* map<string, proto.DpsTestResult> dps_results */ 1:
                     this.binaryReadMap1(message.dpsResults, reader, options);
                     break;
@@ -82,6 +200,38 @@ class TestSuiteResult$Type extends MessageType {
             }
         }
         return message;
+    }
+    binaryReadMap2(map, reader, options) {
+        let len = reader.uint32(), end = reader.pos + len, key, val;
+        while (reader.pos < end) {
+            let [fieldNo, wireType] = reader.tag();
+            switch (fieldNo) {
+                case 1:
+                    key = reader.string();
+                    break;
+                case 2:
+                    val = CharacterStatsTestResult.internalBinaryRead(reader, reader.uint32(), options);
+                    break;
+                default: throw new globalThis.Error("unknown map entry field for field proto.TestSuiteResult.character_stats_results");
+            }
+        }
+        map[key ?? ""] = val ?? CharacterStatsTestResult.create();
+    }
+    binaryReadMap3(map, reader, options) {
+        let len = reader.uint32(), end = reader.pos + len, key, val;
+        while (reader.pos < end) {
+            let [fieldNo, wireType] = reader.tag();
+            switch (fieldNo) {
+                case 1:
+                    key = reader.string();
+                    break;
+                case 2:
+                    val = StatWeightsTestResult.internalBinaryRead(reader, reader.uint32(), options);
+                    break;
+                default: throw new globalThis.Error("unknown map entry field for field proto.TestSuiteResult.stat_weights_results");
+            }
+        }
+        map[key ?? ""] = val ?? StatWeightsTestResult.create();
     }
     binaryReadMap1(map, reader, options) {
         let len = reader.uint32(), end = reader.pos + len, key, val;
@@ -100,6 +250,20 @@ class TestSuiteResult$Type extends MessageType {
         map[key ?? ""] = val ?? DpsTestResult.create();
     }
     internalBinaryWrite(message, writer, options) {
+        /* map<string, proto.CharacterStatsTestResult> character_stats_results = 2; */
+        for (let k of Object.keys(message.characterStatsResults)) {
+            writer.tag(2, WireType.LengthDelimited).fork().tag(1, WireType.LengthDelimited).string(k);
+            writer.tag(2, WireType.LengthDelimited).fork();
+            CharacterStatsTestResult.internalBinaryWrite(message.characterStatsResults[k], writer, options);
+            writer.join().join();
+        }
+        /* map<string, proto.StatWeightsTestResult> stat_weights_results = 3; */
+        for (let k of Object.keys(message.statWeightsResults)) {
+            writer.tag(3, WireType.LengthDelimited).fork().tag(1, WireType.LengthDelimited).string(k);
+            writer.tag(2, WireType.LengthDelimited).fork();
+            StatWeightsTestResult.internalBinaryWrite(message.statWeightsResults[k], writer, options);
+            writer.join().join();
+        }
         /* map<string, proto.DpsTestResult> dps_results = 1; */
         for (let k of Object.keys(message.dpsResults)) {
             writer.tag(1, WireType.LengthDelimited).fork().tag(1, WireType.LengthDelimited).string(k);
