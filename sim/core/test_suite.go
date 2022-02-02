134027ccd08840d266fc53064d6e7910cd7691f3
Ben Echols <lologarithm@gmail.com>
2022-02-02 12:55:47 -0700

    remove commented log

--- sim/core/test_suite.go
+++ sim/core/test_suite.go
@@ -133,7 +133,6 @@ func RunTestSuite(t *testing.T, suiteName string, generator TestGenerator) {
 		}
 		currentTestName = testName
 
-		// log.Printf("TestName: %s", currentTestName)
 		t.Run(currentTestName, func(t *testing.T) {
 			fullTestName := suiteName + "-" + testName
 			if csr != nil {
