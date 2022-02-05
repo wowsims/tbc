5d2c91712df7195ecb85dc1036968f3dca1226a5
jimmyt857 <jimmyt857@gmail.com>
2022-02-05 21:10:25 +0000

    Deploying to gh-pages from @ wowsims/tbc@ef913da350b7ea7758317b66078be2d956070a99 🚀

--- core/proto_utils/action_id.js
+++ core/proto_utils/action_id.js
@@ -145,7 +145,6 @@ export class ActionId {
                 }
                 break;
             case 'Lightning Speed':
-            case 'Siphon Essence':
             case 'Windfury Weapon':
                 if (this.tag == 1) {
                     name += ' (Main Hand)';
