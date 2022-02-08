0856bef57e8a46bb2a751d5920ac199d83e336f9
James Tanner <jimmyt857@gmail.com>
2022-02-08 08:41:48 -0800

    Update hunter presets

--- ui/hunter/presets.ts
+++ ui/hunter/presets.ts
@@ -30,7 +30,7 @@ import * as Tooltips from '/tbc/core/constants/tooltips.js';
 // https://tbc.wowhead.com/talent-calc and copy the numbers in the url.
 export const BeastMasteryTalents = {
 	name: 'BM',
-	data: '502030015150122431051-0505201205',
+	data: '512002015150122431051-0505201205',
 };
 
 export const MarksmanTalents = {
@@ -46,8 +46,8 @@ export const SurvivalTalents = {
 export const DefaultRotation = HunterRotation.create({
 	useMultiShot: true,
 	useArcaneShot: true,
-	viperStartManaPercent: 0.2,
-	viperStopManaPercent: 0.4,
+	viperStartManaPercent: 0.1,
+	viperStopManaPercent: 0.3,
 
 	meleeWeave: true,
 	useRaptorStrike: true,
@@ -685,7 +685,7 @@ export const P3_SV_PRESET = {
 				enchant: Enchants.WEAPON_GREATER_AGILITY,
 			}),
 			ItemSpec.create({
-				id: 32336, // Black Bow of the Betrayer
+				id: 30906, // Bristleblitz Striker
 				enchant: Enchants.STABILIZED_ETERNIUM_SCOPE,
 			}),
 		],
