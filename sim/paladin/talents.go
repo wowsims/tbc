1dd868bd4fcf433e95c660b3c58f87720e9a114e
Michael Coppinger <michaelecoppinger@gmail.com>
2022-02-11 13:46:38 -0800

    Setup for implementing talents and implementation of Benediction

---
+++ sim/paladin/talents.go
@@ -0,0 +1,13 @@
+package paladin
+
+// import (
+// 	"time"
+
+// 	"github.com/wowsims/tbc/sim/core"
+// 	"github.com/wowsims/tbc/sim/core/proto"
+// 	"github.com/wowsims/tbc/sim/core/stats"
+// )
+
+func (paladin *Paladin) applyTalents() {
+
+}
