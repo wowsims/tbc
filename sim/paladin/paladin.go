0c0ced3a8fc5ff9a3d187b9f91073a89cd494741
Michael Coppinger <michaelecoppinger@gmail.com>
2022-02-11 08:42:50 -0800

    Added base stats for all races

--- sim/paladin/paladin.go
+++ sim/paladin/paladin.go
@@ -99,14 +99,47 @@ func NewPaladin(character core.Character, talents proto.PaladinTalents) *Paladin
 
 func init() {
 	core.BaseStats[core.BaseStatsKey{Race: proto.Race_RaceBloodElf, Class: proto.Class_ClassPaladin}] = stats.Stats{
-		stats.Strength:  123,
-		stats.Agility:   79,
 		stats.Stamina:   118,
 		stats.Intellect: 87,
-		stats.Spirit:    88,
 		stats.Mana:      2953,
-		stats.SpellCrit:   139.76,
+		stats.Spirit:    88,
+		stats.Strength:  123,
 		stats.AttackPower: 190,
-		stats.MeleeCrit:   77.06,
+		stats.Agility:   79,
+		stats.MeleeCrit:   14.35,
+		stats.SpellCrit:   73.69, 
+	}
+	core.BaseStats[core.BaseStatsKey{Race: proto.Race_RaceDraenei, Class: proto.Class_ClassPaladin}] = stats.Stats{
+		stats.Stamina:   119,
+		stats.Intellect: 84,
+		stats.Mana:      2953,
+		stats.Spirit:    91,
+		stats.Strength:  127,
+		stats.AttackPower: 190,
+		stats.Agility:   74,
+		stats.MeleeCrit:   14.35,
+		stats.SpellCrit:   73.69, 
+	}
+	core.BaseStats[core.BaseStatsKey{Race: proto.Race_RaceHuman, Class: proto.Class_ClassPaladin}] = stats.Stats{
+		stats.Stamina:   120,
+		stats.Intellect: 83,
+		stats.Mana:      2953,
+		stats.Spirit:    97,
+		stats.Strength:  126,
+		stats.AttackPower: 190,
+		stats.Agility:   77,
+		stats.MeleeCrit:   14.35,
+		stats.SpellCrit:   73.69, 
+	}
+	core.BaseStats[core.BaseStatsKey{Race: proto.Race_RaceDwarf, Class: proto.Class_ClassPaladin}] = stats.Stats{
+		stats.Stamina:   123,
+		stats.Intellect: 82,
+		stats.Mana:      2953,
+		stats.Spirit:    88,
+		stats.Strength:  128,
+		stats.AttackPower: 190,
+		stats.Agility:   73,
+		stats.MeleeCrit:   14.35,
+		stats.SpellCrit:   73.69, 
 	}
 }
