6bba23f118b86eaf007f7eda8ff91062a90f3777
James Tanner <jimmyt857@gmail.com>
2022-02-24 10:36:09 -0800

    Add 70 upgrades importer, still need to do talents

--- ui/core/components/individual_importer.ts
+++ ui/core/components/individual_importer.ts
@@ -1,5 +1,6 @@
 import { Class } from '/tbc/core/proto/common.js';
 import { EquipmentSpec } from '/tbc/core/proto/common.js';
+import { ItemSpec } from '/tbc/core/proto/common.js';
 import { Race } from '/tbc/core/proto/common.js';
 import { Spec } from '/tbc/core/proto/common.js';
 import { IndividualSimUI } from '/tbc/core/individual_sim_ui.js';
@@ -23,10 +24,25 @@ export class IndividualImporter<SpecType extends Spec> extends Component {
 		this.rootElem.id = 'individualImporter';
 		this.rootElem.innerHTML = `
 			<ul class="nav nav-tabs individual-importer-tabs">
-				<li class="individual-importer-tab active"><a data-toggle="tab" href="#addonTab">Addon</a></li>
+				<li class="individual-importer-tab active"><a data-toggle="tab" href="#70upgradesTab">70 Upgrades</a></li>
+				<li class="individual-importer-tab"><a data-toggle="tab" href="#addonTab">Addon</a></li>
 			</ul>
 			<div class="tab-content individual-importer-contents">
-				<div id="addonTab" class="tab-pane individual-importer-tab-content active in">
+				<div id="70upgradesTab" class="tab-pane individual-importer-tab-content fade active in">
+					<div class="individual-importer-description">
+						<p>
+							Import settings from <a href="https://seventyupgrades.com" target="_blank">Seventy Upgrades</a>.
+						</p>
+						<p>
+							This feature imports gear, race, and (optionally) talents. It does NOT import buffs, debuffs, consumes, rotation, or custom stats.
+						</p>
+						<p>
+							To import, paste the output from the site's export option below and click, 'Import'.
+						</p>
+					</div>
+					<textarea class="individual-importer-textarea seventy-upgrades-input"></textarea>
+				</div>
+				<div id="addonTab" class="tab-pane individual-importer-tab-content fade">
 					<div class="individual-importer-description">
 						<p>
 							Import settings from the <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">WoWSims Importer In-Game Addon</a>.
@@ -65,12 +81,68 @@ export class IndividualImporter<SpecType extends Spec> extends Component {
 			},
 		});
 
+		this.setup70UpgradesImport();
 		this.setupAddonImport();
   }
 
+	private setup70UpgradesImport() {
+		const seventyUpgradesContent = document.getElementById('70upgradesTab') as HTMLElement;
+    const seventyUpgradesInput = this.rootElem.getElementsByClassName('seventy-upgrades-input')[0] as HTMLTextAreaElement;
+		this.importButton.addEventListener('click', event => {
+			if (!seventyUpgradesContent.classList.contains('active')) {
+				return
+			}
+
+			try {
+				const importJson = JSON.parse(seventyUpgradesInput.value);
+
+				// Parse all the settings.
+				const charClass = nameToClass((importJson.character.gameClass as string) || '');
+				if (charClass == Class.ClassUnknown) {
+					throw new Error('Could not parse Class!');
+				}
+				const playerClass = this.simUI.player.getClass();
+				if (charClass != playerClass) {
+					throw new Error(`Wrong Class! Expected ${classNames[playerClass]} but found ${classNames[charClass]}!`);
+				}
+
+				const race = nameToRace((importJson.character.race as string) || '');
+				if (race == Race.RaceUnknown) {
+					throw new Error('Could not parse Race!');
+				}
+
+				//const talentsStr = (importJson['talents'] as string) || '';
+
+				let equipmentSpec = EquipmentSpec.create();
+				(importJson.items as Array<any>).forEach(itemJson => {
+					let itemSpec = ItemSpec.create();
+					itemSpec.id = itemJson.id;
+					if (itemJson.enchant?.id) {
+						itemSpec.enchant = itemJson.enchant.id;
+					}
+					if (itemJson.gems) {
+						itemSpec.gems = (itemJson.gems as Array<any>).filter(gemJson => gemJson?.id).map(gemJson => gemJson.id);
+					}
+					equipmentSpec.items.push(itemSpec);
+				});
+
+				const gear = this.simUI.sim.lookupEquipmentSpec(equipmentSpec);
+
+				this.finishImport(race, equipmentSpec, '');
+			} catch (error) {
+				alert('Error importing from seventyUpgrades: ' + error);
+			}
+		});
+	}
+
 	private setupAddonImport() {
+		const addonContent = document.getElementById('addonTab') as HTMLElement;
     const addonInput = this.rootElem.getElementsByClassName('addon-input')[0] as HTMLTextAreaElement;
 		this.importButton.addEventListener('click', event => {
+			if (!addonContent.classList.contains('active')) {
+				return
+			}
+
 			try {
 				const importJson = JSON.parse(addonInput.value);
 
@@ -90,46 +162,55 @@ export class IndividualImporter<SpecType extends Spec> extends Component {
 				}
 
 				const talentsStr = (importJson['talents'] as string) || '';
-
-				const missingEnchants: Array<number> = [];
-
 				const equipmentSpec = EquipmentSpec.fromJson(importJson['gear']);
-				equipmentSpec.items.forEach(item => {
-					if (item.enchant) {
-						const enchant = this.simUI.sim.getEnchantFlexible(item.enchant);
-						if (enchant) {
-							item.enchant = enchant.id;
-						} else {
-							missingEnchants.push(item.enchant);
-						}
-					}
-				});
-				const gear = this.simUI.sim.lookupEquipmentSpec(equipmentSpec);
 
-				const expectedItemIds = (importJson['gear'].items as Array<any>).map(item => item.id) as Array<number>;
-				const foundItemIds = gear.asSpec().items.map(item => item.id);
-				const missingItems = expectedItemIds.filter(expectedId => !foundItemIds.includes(expectedId));
-
-				// Now update settings using the parsed values.
-				const eventID = TypedEvent.nextEventID();
-				TypedEvent.freezeAllAndDo(() => {
-					this.simUI.player.setRace(eventID, race);
-					this.simUI.player.setTalentsString(eventID, talentsStr);
-					this.simUI.player.setGear(eventID, gear);
-				});
-
-				$('#individualImporter').bPopup().close();
-
-				if (missingItems.length == 0 && missingEnchants.length == 0) {
-					alert('Import successful!');
-				} else {
-					alert('Import successful, but the following IDs were not found in the sim database:' + 
-							(missingItems.length == 0 ? '' : '\n\nItems: ' + missingItems.join(', ')) +
-							(missingEnchants.length == 0 ? '' : '\n\nEnchants: ' + missingEnchants.join(', ')));
-				}
+				this.finishImport(race, equipmentSpec, talentsStr);
 			} catch (error) {
 				alert('Error importing from addon: ' + error);
 			}
 		});
+	}
+
+	private finishImport(race: Race, equipmentSpec: EquipmentSpec, talentsStr: string) {
+		equipmentSpec.items.forEach(item => {
+			if (item.enchant) {
+				const dbEnchant = this.simUI.sim.getEnchantFlexible(item.enchant);
+				if (dbEnchant) {
+					item.enchant = dbEnchant.id;
+				} else {
+					item.enchant = 0;
+				}
+			}
+		});
+
+		const gear = this.simUI.sim.lookupEquipmentSpec(equipmentSpec);
+
+		const expectedEnchantIds = equipmentSpec.items.map(item => item.enchant);
+		const foundEnchantIds = gear.asSpec().items.map(item => item.enchant);
+		const missingEnchants = expectedEnchantIds.filter(expectedId => expectedId != 0 && !foundEnchantIds.includes(expectedId));
+
+		const expectedItemIds = equipmentSpec.items.map(item => item.id);
+		const foundItemIds = gear.asSpec().items.map(item => item.id);
+		const missingItems = expectedItemIds.filter(expectedId => !foundItemIds.includes(expectedId));
+
+		// Now update settings using the parsed values.
+		const eventID = TypedEvent.nextEventID();
+		TypedEvent.freezeAllAndDo(() => {
+			this.simUI.player.setRace(eventID, race);
+			this.simUI.player.setGear(eventID, gear);
+			if (talentsStr) {
+				this.simUI.player.setTalentsString(eventID, talentsStr);
+			}
+		});
+
+		$('#individualImporter').bPopup().close();
+
+		if (missingItems.length == 0 && missingEnchants.length == 0) {
+			alert('Import successful!');
+		} else {
+			alert('Import successful, but the following IDs were not found in the sim database:' + 
+					(missingItems.length == 0 ? '' : '\n\nItems: ' + missingItems.join(', ')) +
+					(missingEnchants.length == 0 ? '' : '\n\nEnchants: ' + missingEnchants.join(', ')));
+		}
 	}
 }
