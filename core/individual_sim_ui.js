b571a6f2f2968e28a12bed7e7938f513dc5e6ada
jimmyt857 <jimmyt857@gmail.com>
2022-03-07 02:36:22 +0000

    Deploying to gh-pages from @ wowsims/tbc@6bf227ab13ec00b2345071c804c73a91ca4fd24c 🚀

--- core/individual_sim_ui.js
+++ core/individual_sim_ui.js
@@ -175,7 +175,7 @@ export class IndividualSimUI extends SimUI {
     }
     addTopbarComponents() {
         const importSettings = document.createElement('span');
-        importSettings.classList.add('import-settings', 'fa', 'fa-upload');
+        importSettings.classList.add('import-settings', 'fas', 'fa-upload');
         tippy(importSettings, {
             'content': 'Import',
             'allowHTML': true,
@@ -187,7 +187,7 @@ export class IndividualSimUI extends SimUI {
             this.addToolbarItem(importSettings);
         }
         const shareLink = document.createElement('span');
-        shareLink.classList.add('share-link', 'fa', 'fa-link', 'within-raid-sim-hide');
+        shareLink.classList.add('share-link', 'fas', 'fa-link', 'within-raid-sim-hide');
         tippy(shareLink, {
             'content': 'Shareable link',
             'allowHTML': true,
