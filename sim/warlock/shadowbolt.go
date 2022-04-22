16e1e7374331bfea1e21eb7d51900e0ce0a35796
Ben Echols <lologarithm@gmail.com>
2022-04-22 09:27:13 -0600

    starting to add shadowbolt

--- sim/warlock/shadowbolt.go
+++ sim/warlock/shadowbolt.go
@@ -21,20 +21,20 @@ var ShadowBolt11ActionID = core.ActionID{SpellID: SpellIDSB11}
 
 func (warlock *Warlock) newShadowboltSpell(sim *core.Simulation) *core.Spell {
 	baseCost := 420.0
-	minBaseDamage := 544.0
-	maxBaseDamage := 607.0
-	spellCoefficient := 0.857
+	// minBaseDamage := 544.0
+	// maxBaseDamage := 607.0
+	// spellCoefficient := 0.857
 
 	// // This seems to be unaffected by wrath of cenarius so it needs to come first.
 	// bonusFlatDamage := core.TernaryFloat64(druid.Equip[items.ItemSlotRanged].ID == IvoryMoongoddess, 55*spellCoefficient, 0)
 	// spellCoefficient += 0.04 * float64(druid.Talents.WrathOfCenarius)
 
 	effect := core.SpellEffect{
-		BonusSpellCritRating: (float64(druid.Talents.FocusedStarlight) * 2 * core.SpellCritRatingPerCritChance) + core.TernaryFloat64(ItemSetThunderheart.CharacterHasSetBonus(&druid.Character, 4), 5*core.SpellCritRatingPerCritChance, 0),
-		DamageMultiplier:     1 + 0.02*float64(druid.Talents.Moonfury),
-		ThreatMultiplier:     1,
-		BaseDamage:           core.BaseDamageConfigMagic(minBaseDamage+bonusFlatDamage, maxBaseDamage+bonusFlatDamage, spellCoefficient),
-		OutcomeApplier:       core.OutcomeFuncMagicHitAndCrit(druid.SpellCritMultiplier(1, 0.2*float64(druid.Talents.Vengeance))),
+		// BonusSpellCritRating: (float64(druid.Talents.FocusedStarlight) * 2 * core.SpellCritRatingPerCritChance) + core.TernaryFloat64(ItemSetThunderheart.CharacterHasSetBonus(&druid.Character, 4), 5*core.SpellCritRatingPerCritChance, 0),
+		// DamageMultiplier:     1 + 0.02*float64(druid.Talents.Moonfury),
+		// ThreatMultiplier:     1,
+		// BaseDamage:           core.BaseDamageConfigMagic(minBaseDamage+bonusFlatDamage, maxBaseDamage+bonusFlatDamage, spellCoefficient),
+		// OutcomeApplier:       core.OutcomeFuncMagicHitAndCrit(druid.SpellCritMultiplier(1, 0.2*float64(druid.Talents.Vengeance))),
 	}
 
 	// if ItemSetNordrassil.CharacterHasSetBonus(&druid.Character, 4) {
@@ -62,17 +62,42 @@ func (warlock *Warlock) newShadowboltSpell(sim *core.Simulation) *core.Spell {
 
 		Cast: core.CastConfig{
 			DefaultCast: core.Cast{
-				Cost:     baseCost * (1 - 0.03*float64(druid.Talents.Moonglow)),
+				Cost:     baseCost, // * (1 - 0.03*float64(druid.Talents.Moonglow)),
 				GCD:      core.GCDDefault,
-				CastTime: time.Millisecond*3500 - (time.Millisecond * 100 * time.Duration(druid.Talents.StarlightWrath)),
+				CastTime: time.Millisecond * 3000, //*3500 - (time.Millisecond * 100 * time.Duration(druid.Talents.StarlightWrath)),
 			},
 
-			ModifyCast: func(_ *core.Simulation, _ *core.Spell, cast *core.Cast) {
-				druid.applyNaturesGrace(cast)
-				druid.applyNaturesSwiftness(cast)
-			},
+			// ModifyCast: func(_ *core.Simulation, _ *core.Spell, cast *core.Cast) {
+			// 	druid.applyNaturesGrace(cast)
+			// 	druid.applyNaturesSwiftness(cast)
+			// },
 		},
 
 		ApplyEffects: core.ApplyEffectFuncDirectDamage(effect),
+	})
+}
+
+func (warlock *Warlock) impShadowboltDebuffAura(target *core.Target) *core.Aura {
+	return target.GetOrRegisterAura(core.Aura{
+		Label:     "Improved Shadow Bolt",
+		ActionID:  ShadowBolt11ActionID,
+		Duration:  time.Second * 10,
+		MaxStacks: 4,
+		OnGain: func(aura *core.Aura, sim *core.Simulation) {
+			target.PseudoStats.ShadowDamageTakenMultiplier *= 1.2
+		},
+		OnExpire: func(aura *core.Aura, sim *core.Simulation) {
+			target.PseudoStats.ShadowDamageTakenMultiplier /= 1.2
+		},
+		OnSpellHit: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, spellEffect *core.SpellEffect) {
+			if spell.SpellSchool != core.SpellSchoolShadow {
+				return
+			}
+			if !spellEffect.Landed() || spellEffect.Damage == 0 {
+				return
+			}
+
+			aura.RemoveStack(sim)
+		},
 	})
 }
