b940263b4edffb265013025da14b3f00ef4ff25a
James Tanner <jimmyt857@gmail.com>
2022-04-23 13:44:31 -0700

    Fix cd bug

--- sim/warrior/protection/protection_warrior.go
+++ sim/warrior/protection/protection_warrior.go
@@ -40,7 +40,9 @@ func NewProtectionWarrior(character core.Character, options proto.Player) *Prote
 	war.EnableRageBar(warOptions.Options.StartingRage, core.TernaryFloat64(war.Talents.EndlessRage, 1.25, 1), func(sim *core.Simulation) {
 		if war.GCD.IsReady(sim) {
 			war.TryUseCooldowns(sim)
-			war.doRotation(sim)
+			if war.GCD.IsReady(sim) {
+				war.doRotation(sim)
+			}
 		}
 	})
 	war.EnableAutoAttacks(war, core.AutoAttackOptions{
