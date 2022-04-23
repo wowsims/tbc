d8a5d79a1453e3dcd9fc3866e2f6dd7a906bf598
Ben Echols <lologarithm@gmail.com>
2022-04-23 09:54:48 -0600

    added green poncho

--- generate_items/writer.go
+++ generate_items/writer.go
@@ -307,4 +307,5 @@ var allowList = map[int]bool{
 	33144: true, // Facet of Eternity
 	6360:  true, // Steelscale Crushfish
 	8345:  true, // Wolfshead Helm
+	28032: true, // Delicate Green Poncho
 }
