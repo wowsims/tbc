360041437133334f7feadc5b07ff4b06e673e294
Ben Echols <lologarithm@gmail.com>
2022-05-03 18:08:50 -0600

    deprecate the right thing

--- ui/raid/presets.ts
+++ ui/raid/presets.ts
@@ -818,7 +818,6 @@ export const buffBotPresets: Array<BuffBotSettings> = [
 		// The value of this field must never change, to preserve local storage data.
 		buffBotId: 'Resto Shaman',
 		spec: Spec.SpecElementalShaman,
-		deprecated: true,
 		name: 'Resto Shaman',
 		tooltip: 'Resto Shaman: Adds Bloodlust, Mana Spring Totem, Wrath of Air Totem, Mana Tide Totem, and Drums of Battle.',
 		iconUrl: talentTreeIcons[Class.ClassShaman][2],
@@ -853,6 +852,7 @@ export const buffBotPresets: Array<BuffBotSettings> = [
 		buffBotId: 'Malediction Warlock',
 		spec: Spec.SpecWarlock,
 		name: 'Aff Warlock',
+		deprecated: true,
 		tooltip: 'Afflication Warlock: Adds Curse of Elements (improved). Also adds +20% uptime to ISB.',
 		iconUrl: 'https://wow.zamimg.com/images/wow/icons/large/spell_shadow_curseofachimonde.jpg',
 		modifyRaidProto: (buffBot: BuffBot, raidProto: RaidProto, partyProto: PartyProto) => {
