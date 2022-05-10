7a86c24d0e08683c4801bd4c982b83111c54c1e9
James Tanner <jimmyt857@gmail.com>
2022-05-09 19:33:53 -0700

    Add remaining set bonuses

--- sim/warrior/items.go
+++ sim/warrior/items.go
@@ -107,9 +107,56 @@ var ItemSetDestroyerArmor = core.ItemSet{
 	Bonuses: map[int32]core.ApplyEffect{
 		2: func(agent core.Agent) {
 			// Each time you use your Shield Block ability, you gain 100 block value against a single attack in the next 6 sec.
+			warrior := agent.(WarriorAgent).GetWarrior()
+			procAura := warrior.RegisterAura(core.Aura{
+				Label:     "Destroyer 2pc Proc",
+				ActionID:  core.ActionID{SpellID: 37522},
+				Duration:  time.Second * 6,
+				MaxStacks: 1,
+				OnGain: func(aura *core.Aura, sim *core.Simulation) {
+					warrior.AddStatDynamic(sim, stats.BlockValue, 100)
+				},
+				OnExpire: func(aura *core.Aura, sim *core.Simulation) {
+					warrior.AddStatDynamic(sim, stats.BlockValue, -100)
+				},
+				OnSpellHitTaken: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, spellEffect *core.SpellEffect) {
+					if spellEffect.Outcome.Matches(core.OutcomeBlock) {
+						aura.RemoveStack(sim)
+					}
+				},
+			})
+
+			warrior.RegisterAura(core.Aura{
+				Label:    "Destroyer 2pc",
+				Duration: core.NeverExpires,
+				OnReset: func(aura *core.Aura, sim *core.Simulation) {
+					aura.Activate(sim)
+				},
+				OnCastComplete: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell) {
+					if spell == warrior.ShieldBlock {
+						procAura.Activate(sim)
+						procAura.SetStacks(sim, 1)
+					}
+				},
+			})
 		},
 		4: func(agent core.Agent) {
 			// You have a chance each time you are hit to gain 200 haste rating for 10 sec.
+			warrior := agent.(WarriorAgent).GetWarrior()
+			procAura := warrior.NewTemporaryStatsAura("Destroyer 4pc Proc", core.ActionID{SpellID: 37525}, stats.Stats{stats.MeleeHaste: 200}, time.Second*10)
+
+			warrior.RegisterAura(core.Aura{
+				Label:    "Destroyer 4pc",
+				Duration: core.NeverExpires,
+				OnReset: func(aura *core.Aura, sim *core.Simulation) {
+					aura.Activate(sim)
+				},
+				OnSpellHitTaken: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, spellEffect *core.SpellEffect) {
+					if spellEffect.Landed() && sim.RandomFloat("Destroyer 4pc") < 0.07 {
+						procAura.Activate(sim)
+					}
+				},
+			})
 		},
 	},
 }
