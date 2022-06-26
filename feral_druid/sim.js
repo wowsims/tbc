a76cc5d4482900e958ed4638677a7b1c786e50b4
jimmyt857 <jimmyt857@gmail.com>
2022-06-26 02:30:41 +0000

    Deploying to gh-pages from @ wowsims/tbc@e9d60144cbc65dc834e4a4284b45ddc2ba59d5f3 🚀

--- feral_druid/sim.js
+++ feral_druid/sim.js
@@ -29,6 +29,16 @@ export class FeralDruidSimUI extends IndividualSimUI {
             cssClass: 'feral-druid-sim-ui',
             // List any known bugs / issues here and they'll be shown on the site.
             knownIssues: [],
+            warnings: [
+                // Warning that a frontal rotation is not implemented.
+                (simUI) => {
+                    return {
+                        updateOn: simUI.player.inFrontOfTargetChangeEmitter,
+                        shouldDisplay: () => simUI.player.getInFrontOfTarget(),
+                        getContent: () => 'Frontal rotation (without Shred) is not implemented.',
+                    };
+                },
+            ],
             // All stats for which EP should be calculated.
             epStats: [
                 Stat.StatStrength,
